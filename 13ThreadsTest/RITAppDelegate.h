8e245f06bbbe63774de4d0f28b6cd02b102964ca
Pronin Alexander <pronin.alx@gmail.com>
2014-02-11 02:35:46 +0200

    Initial Commit

---
+++ 13ThreadsTest/RITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RITAppDelegate.h
+//  13ThreadsTest
+//
+//  Created by Aleksandr Pronin on 11.02.14.
+//  Copyright (c) 2014 Aleksandr Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 13ThreadsTestTests/_3ThreadsTestTests.m
@@ -0,0 +1,34 @@
+//
+//  _3ThreadsTestTests.m
+//  13ThreadsTestTests
+//
+//  Created by Aleksandr Pronin on 11.02.14.
+//  Copyright (c) 2014 Aleksandr Pronin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _3ThreadsTestTests : XCTestCase
+
+@end
+
+@implementation _3ThreadsTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
